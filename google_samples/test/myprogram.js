82d5004fb64bd56da3bf3a2e63e10b7c53fa0e26
yaoguangfiles <yaoguangfiles@gmail.com>
2022-11-16 22:07:31 -0500

    first commit

---
+++ google_samples/test/myprogram.js
@@ -0,0 +1,8 @@
+
+// from: https://phoenixnap.com/kb/install-node-js-npm-on-windows
+
+var http = require('http');
+http.createServer(function (req, res) {
+res.writeHead(200, {'Content-Type': 'text/html'});
+res.end('Hello World!');
+}).listen(8080);
